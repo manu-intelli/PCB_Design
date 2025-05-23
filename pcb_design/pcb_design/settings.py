90f63b37e8ad1133f041c53d9873886e360c0de8
Sathish <onnmanbin95@gmail.com>
2025-05-23 11:35:57 +0530

    setting updated pagination

--- pcb_design/pcb_design/settings.py
+++ pcb_design/pcb_design/settings.py
@@ -131,6 +131,14 @@ REST_FRAMEWORK = {
     ),
 }
 
+# settings.py
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5  # You can change this number based on your preference
+}
+
+
 # JWT token expiration settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=150),  # Access token lifetime
