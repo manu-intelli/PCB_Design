94805845def068b349f1af8e78d1f0b98bdc6fbf
Sathish <onnmanbin95@gmail.com>
2025-06-26 09:04:10 +0530

    admin model added in thelifi

--- pcb_design/thelifi/models.py
+++ pcb_design/thelifi/models.py
@@ -1,4 +1,6 @@
+import os
 from django.db import models
+from django.conf import settings
 
 class FilterSubmission(models.Model):
     folder_name = models.CharField(max_length=255, unique=True)
@@ -9,3 +11,24 @@ class FilterSubmission(models.Model):
 
     def __str__(self):
         return self.folder_name
+
+    @property
+    def folder_path(self):
+        return f"uploads/{self.folder_name}"
+
+    def list_files_in_folder(self):
+        folder_full_path = os.path.join(settings.MEDIA_ROOT, 'uploads', self.folder_name)
+        if not os.path.exists(folder_full_path):
+            return "Folder not found."
+
+        file_list = []
+        for root, dirs, files in os.walk(folder_full_path):
+            for file in files:
+                rel_path = os.path.relpath(os.path.join(root, file), settings.MEDIA_ROOT)
+                file_list.append(f"/media/{rel_path}")
+
+        if file_list:
+            return '\n'.join(file_list)
+        return "No files found."
+
+    list_files_in_folder.short_description = "Files in Folder"
